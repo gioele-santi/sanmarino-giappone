e9947000de16f7a3a5efdac3295499f72680c14d
gioele <gioele.santi@gmail.com>
2020-10-25 17:54:49 +0100

    Changed site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Associazione San Marino - Giappone',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Benvenuti sul sito dell\'Associazione San Marino - Giappone ',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
