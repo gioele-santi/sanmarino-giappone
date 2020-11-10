013a605c4db629b3e214d5819f11f4884c9f3fb8
gioele <gioele.santi@gmail.com>
2020-11-10 09:28:35 +0100

    Create index.js
    Restored blog page

---
+++ src/pages/blog/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import BlogRoll from '../../components/BlogRoll'
+
+export default class BlogIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/blog-index.jpg')`,
+          }}
+        >
+          <h1
+            className="over-image-title has-text-weight-bold is-size-1"
+          >
+            Notizie
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <BlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
