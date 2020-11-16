ee77cbe82f832af218f635aa960cfcca7bc753ad
gioele <gioele.santi@gmail.com>
2020-11-17 00:36:15 +0100

    Update blog-list-template.js
    Added the rest of framing for post list

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -13,53 +13,71 @@ export default class BlogList extends React.Component {
           const title = node.frontmatter.title || node.fields.slug
           return <div key={node.fields.slug}>{title}</div>
         })} */}
-
-
-<div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        }}
-                      />
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/blog-index.jpg')`,
+          }}
+        >
+          <h1
+            className="over-image-title has-text-weight-bold is-size-1"
+          >
+            Notizie
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+            <div className="columns is-multiline">
+                {posts &&
+                  posts.map(({ node: post }) => (
+                    <div className="is-parent column is-6" key={post.id}>
+                      <article
+                        className={`blog-list-item tile is-child box notification ${
+                          post.frontmatter.featuredpost ? 'is-featured' : ''
+                        }`}
+                      >
+                        <header>
+                          {post.frontmatter.featuredimage ? (
+                            <div className="featured-thumbnail">
+                              <PreviewCompatibleImage
+                                imageInfo={{
+                                  image: post.frontmatter.featuredimage,
+                                  alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                                }}
+                              />
+                            </div>
+                          ) : null}
+                          <p className="post-meta">
+                            <Link
+                              className="title has-text-primary is-size-4"
+                              to={post.fields.slug}
+                            >
+                              {post.frontmatter.title}
+                            </Link>
+                            <span> &bull; </span>
+                            <span className="subtitle is-size-5 is-block">
+                              {post.frontmatter.date}
+                            </span>
+                          </p>
+                        </header>
+                        <p>
+                          {post.excerpt}
+                          <br />
+                          <br />
+                          <Link className="button" to={post.fields.slug}>
+                            Leggi tutto →
+                          </Link>
+                        </p>
+                      </article>
                     </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Leggi tutto →
-                  </Link>
-                </p>
-              </article>
+                  ))}
+              </div>
             </div>
-          ))}
-      </div>
+          </div>
+        </section>
+
+
 
 
       </Layout>
