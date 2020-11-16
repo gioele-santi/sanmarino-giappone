047b94347548de156cd96a00736666fed555b9ae
gioele <gioele.santi@gmail.com>
2020-11-16 23:43:15 +0100

    Update blog-list-template.js
    Added boxes from blogroll.
    I need to fix preview images and article start (probably due to query)
    And I need to add header and the whole blogroll container and pagination.

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 import Layout from "../components/layout"
 
 
@@ -8,10 +9,59 @@ export default class BlogList extends React.Component {
     const posts = this.props.data.allMarkdownRemark.edges
     return (
       <Layout>
-        {posts.map(({ node }) => {
+        {/* {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return <div key={node.fields.slug}>{title}</div>
-        })}
+        })} */}
+
+
+<div className="columns is-multiline">
+        {posts &&
+          posts.map(({ node: post }) => (
+            <div className="is-parent column is-6" key={post.id}>
+              <article
+                className={`blog-list-item tile is-child box notification ${
+                  post.frontmatter.featuredpost ? 'is-featured' : ''
+                }`}
+              >
+                <header>
+                  {post.frontmatter.featuredimage ? (
+                    <div className="featured-thumbnail">
+                      <PreviewCompatibleImage
+                        imageInfo={{
+                          image: post.frontmatter.featuredimage,
+                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                        }}
+                      />
+                    </div>
+                  ) : null}
+                  <p className="post-meta">
+                    <Link
+                      className="title has-text-primary is-size-4"
+                      to={post.fields.slug}
+                    >
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <span className="subtitle is-size-5 is-block">
+                      {post.frontmatter.date}
+                    </span>
+                  </p>
+                </header>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button" to={post.fields.slug}>
+                    Leggi tutto →
+                  </Link>
+                </p>
+              </article>
+            </div>
+          ))}
+      </div>
+
+
       </Layout>
     )
   }
