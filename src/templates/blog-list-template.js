89edd0881b18e81c9cab470089b744c7b9601a4f
gioele <gioele.santi@gmail.com>
2020-11-17 23:25:34 +0100

    Update blog-list-template.js
    Fixed previous page

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -11,7 +11,7 @@ export default class BlogList extends React.Component {
     const { currentPage, numPages } = this.props.pageContext
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
-    const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+    const prevPage = endpoint + (currentPage - 1 === 1 ? '' : `/` + (currentPage - 1).toString())
     const nextPage = endpoint + `/` + (currentPage + 1).toString()
     return (
       <Layout>
