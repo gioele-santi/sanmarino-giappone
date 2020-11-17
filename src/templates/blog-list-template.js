70ce71df4a8da4b68241a77ec19be8efc6b7379a
gioele <gioele.santi@gmail.com>
2020-11-17 13:06:06 +0100

    Update blog-list-template.js
    Fixed typo that prevented deploy online

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 
 export default class BlogList extends React.Component {
