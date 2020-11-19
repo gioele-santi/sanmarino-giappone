b767e27b61020c7db1a9791510bab753f14d9695
gioele <gioele.santi@gmail.com>
2020-11-19 21:07:25 +0100

    Update Navbar.js
    Fixed img link name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
+            <Link to="/" className="navbar-item" title="Home">
               <img src={logo} alt="sanmarino-giappone" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
