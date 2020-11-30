1cb3c3bf51e8d4fcce7ff03a9723dd5e957a7130
gioele <gioele.santi@gmail.com>
2020-11-30 15:12:14 +0100

    Added new responsive style
    Inserted new style in main page for test. Upload in production. Desktop is ok, mobile can't get nay worse than now.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -17,13 +17,13 @@ export const IndexPageTemplate = ({
 }) => (
   <div>
     <div
-      className="full-width-image margin-top-0"
+      className="full-width-image margin-top-0 header-image"
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+        // backgroundPosition: `top left`,
+        // backgroundAttachment: `fixed`,
       }}
     >
       <div
